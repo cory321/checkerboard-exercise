1283f8df045d0c6a62afc8eb7eec8a9075c2e248
cory321 <cory321@gmail.com>
2015-08-17 15:12:28 -0700

    black and red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 // Your JS goes here
+
+var body, box, content, numBoxes;
+
+window.onload = function() {
+	generateGrid();
+};
+
+var generateGrid = function() {
+
+	//Generate the Grid
+
+	body = document.getElementsByTagName("body")[0];
+	content = "<div class=\"box\"></div>";
+	numBoxes = 81; // 9x9 Grid
+
+	for(var i = 0; i < numBoxes; i++) {
+		body.innerHTML = body.innerHTML + content;
+	}
+
+	box = document.querySelectorAll(".box");
+
+	//Set Styles
+
+	body.style.width = "900px";
+	body.style.margin = "0";
+
+	for (var i = 0; i < numBoxes; i++) {
+		box[i].style.width = "100px";
+		box[i].style.height = "100px";
+		box[i].style.backgroundColor = "black";
+		box[i].style.float = "left";
+	};
+
+	for (var i = 0; i < numBoxes; i++) {
+		if (i % 2 == 0) {
+			box[i].style.backgroundColor = "red";
+		};
+	};
+};
+
